6d2dee2a69f7be8172f2239f577f56d93d7a11f5
Rens Holmer <rens.holmer@wur.nl>
2019-07-01 13:19:47 +0200

    more tests

--- tests/sequence_tests.py
+++ tests/sequence_tests.py
@@ -1,5 +1,12 @@
 from picea import SequenceCollection
 
+def test_empty_init():
+    seq = SequenceCollection()
+
+def test_parsing():
+    fasta = '>A\nABC\n>B\nDEF'
+    seq = SequenceCollection.from_fasta(string = fasta)
+
 def test_input_output():
     fasta = '>A\nABC\n>B\nDEF'
     seq = SequenceCollection.from_fasta(string = fasta)
