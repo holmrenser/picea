7332d71294d0b0296d561220bdd100a0f3a05752
Rens Holmer <rens.holmer@wur.nl>
2019-07-01 09:53:52 +0200

    more tests

---
+++ tests/sequence_tests.py
@@ -0,0 +1,11 @@
+from picea import SequenceCollection
+
+def test_input_output():
+    fasta = '>A\nABC\n>B\nDEF'
+    seq = SequenceCollection.from_fasta(string = fasta)
+    assert seq.to_fasta() == fasta
+
+def test_align():
+    fasta = '>A\nABC\n>B\nDEF'
+    seq = SequenceCollection.from_fasta(string = fasta)
+    seq.align()
