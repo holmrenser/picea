5f41e8635b60f3a36bb440e680dd8094bd3d7a73
Rens Holmer <rens.holmer@wur.nl>
2019-07-01 12:09:33 +0200

    fail test again

--- tests/sequence_tests.py
+++ tests/sequence_tests.py
@@ -4,8 +4,8 @@ def test_input_output():
     fasta = '>A\nABC\n>B\nDEF'
     seq = SequenceCollection.from_fasta(string = fasta)
     assert seq.to_fasta() == fasta
-'''
+
 def test_align():
     fasta = '>A\nABC\n>B\nDEF'
     seq = SequenceCollection.from_fasta(string = fasta)
-    seq.align()'''
+    seq.align()
