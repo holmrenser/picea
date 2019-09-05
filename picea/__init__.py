607023c5129a623b12844df3e6afb018799e0ed8
Holmer, Rens <rens.holmer@wur.nl>
2019-09-05 11:43:24 +0200

    flake8

--- picea/__init__.py
+++ picea/__init__.py
@@ -1,4 +1,4 @@
 __author__ = 'rensholmer'
 
 from .tree import Tree  # noqa
-from .sequence import SequenceCollection, SequenceAnnotation, Interval  # noqa
+from .sequence import SequenceCollection, SequenceAnnotation, SequenceInterval  # noqa
--- picea/sequence.py
+++ picea/sequence.py
@@ -61,8 +61,8 @@ class SequenceCollection(dict):
         Returns:
             SequenceCollection -- SequenceCollection instance
         """
-        assert(filename or string)
-        assert(not filename and string)
+        assert filename or string
+        assert not (filename and string)
         sequencecollection = cls()
         if filename:
             with open(filename) as filehandle:
@@ -108,40 +108,91 @@ class SequenceCollection(dict):
 
 class SequenceAnnotation():
     def __init__(self, sequence_collection=None):
+        """[summary]
+
+        Args:
+            sequence_collection ([type], optional): [description].
+                Defaults to None.
+        """
         self.sequence_collection = sequence_collection
         self._intervals = dict()
         self._index = dict()
 
+    def __getitem__(self, key):
+        return self._intervals[key]
+
+    def __setitem__(self, key, value):
+        if key in self._intervals:
+            raise Exception('Duplicate ID')
+        self._intervals[key] = value
+
+    def __iter__(self):
+        yield from self._intervals.values()
+
     @classmethod
     def from_gff(cls, filename=None, string=None,
                  sequence_collection=None):
-        assert(filename or string)
-        assert(not filename and string)
+        """[summary]
+
+        Args:
+            filename ([type], optional): [description]. Defaults to None.
+            string ([type], optional): [description]. Defaults to None.
+            sequence_collection ([type], optional): [description].
+                Defaults to None.
+
+        Returns:
+            [type]: [description]
+        """
+        assert filename or string
+        assert not (filename and string)
         sequence_annotation = cls(sequence_collection=sequence_collection)
         if filename:
             with open(filename) as filehandle:
                 string = filehandle.read()
         for line_number, line in enumerate(string.split('\n')):
-            interval = Interval.from_gff_line(gff_line=line,
-                                              line_number=line_number)
+            line = line.strip()
+            if not line:
+                continue
+            if line[0] == '#':
+                continue
+            interval = SequenceInterval.from_gff_line(gff_line=line,
+                                                      line_number=line_number)
             sequence_annotation._intervals[interval.ID] = interval
 
-        for interval in sequence_annotation._intervals.values():
-            for parentID in interval.parent:
-                parent = sequence_annotation._intervals[parentID]
-                parent.children.append(interval.ID)
+        for interval in sequence_annotation:
+            if interval.parent:
+                for parent_ID in interval.parent:
+                    parent = sequence_annotation[parent_ID]
+                    parent.children.append(interval.ID)
 
         return sequence_annotation
 
 
-class Interval():
+class SequenceInterval():
     _predefined_attributes = ('ID', 'name', 'alias', 'parent', 'target',
                               'gap', 'derives_from', 'note', 'dbxref',
                               'ontology_term', 'is_circular')
 
     def __init__(self, seqid=None, source=None, interval_type=None,
                  start=None, end=None, score=None, strand=None, phase=None,
-                 attributes=None, children=[], **kwargs):
+                 attributes=None, children=None, **kwargs):
+        """[summary]
+
+        Args:
+            seqid ([type], optional): [description]. Defaults to None.
+            source ([type], optional): [description]. Defaults to None.
+            interval_type ([type], optional): [description]. Defaults to None.
+            start ([type], optional): [description]. Defaults to None.
+            end ([type], optional): [description]. Defaults to None.
+            score ([type], optional): [description]. Defaults to None.
+            strand ([type], optional): [description]. Defaults to None.
+            phase ([type], optional): [description]. Defaults to None.
+            attributes ([type], optional): [description]. Defaults to None.
+            children ([type], optional): [description]. Defaults to None.
+
+        Raises:
+            NotImplementedError: [description]
+        """
         # Standard gff fields
         self.seqid = seqid
         self.source = source
@@ -156,17 +207,50 @@ class Interval():
         # Attributes with predefined meanings in the gff spec
         for attr in self._predefined_attributes:
             self[attr] = kwargs.get(attr, None)
+
         for key in kwargs.keys():
             if key not in self._predefined_attributes:
                 raise NotImplementedError(f'{key} is not a valid attribute')
 
         # Additional fields, used internally
+        if children is None:
+            children = []
         self.children = children
+
+    def __repr__(self):
+        return (
+            f'<{self.interval_type} SequenceInterval '
+            f'{self.ID} '
+            f'{self.start}..{self.end}..{self.strand} '
+            f'at {hex(id(self))}'
+        )
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
 
     @classmethod
     def from_gff_line(cls, gff_line=None, line_number=None):
+        """[summary]
+
+        Args:
+            gff_line ([type], optional): [description]. Defaults to None.
+            line_number ([type], optional): [description]. Defaults to None.
+
+        Raises:
+            ValueError: [description]
+            ValueError: [description]
+            ValueError: [description]
+            ValueError: [description]
+            ValueError: [description]
+
+        Returns:
+            [type]: [description]
+        """
         gff_parts = gff_line.split('\t')
-        assert len(gff_parts) == 9
+        assert len(gff_parts) == 9, gff_parts
         seqid, source, interval_type, start, end,\
             score, strand, phase = gff_parts[:8]
         try:
@@ -177,13 +261,15 @@ class Interval():
             if line_number:
                 error = f'{error}, gff line {line_number}'
             raise ValueError(error)
-        try:
-            score = float(score)
-        except ValueError:
-            error = 'GFF score field must be a float'
-            if line_number:
-                error = f'{error}, gff line {line_number}'
-            raise ValueError(error)
+
+        if score != '.':
+            try:
+                score = float(score)
+            except ValueError:
+                error = 'GFF score field must be a float'
+                if line_number:
+                    error = f'{error}, gff line {line_number}'
+                raise ValueError(error)
 
         if strand not in ('+', '-', '.'):
             error = 'GFF strand must be one of "+", "-" or "."'
@@ -197,12 +283,13 @@ class Interval():
                 error = f'{error}, gff line {line_number}'
             raise ValueError(error)
 
-        if interval_type == 'CDS' and phase not in ('0', '1', '2'):
-            error = 'GFF intervals of type CDS must have phase of\
-                "0", "1" or "2"'
-            if line_number:
-                error = f'{error}, gff line {line_number}'
-                raise ValueError(error)
+        # Disable phase checking of CDS for now...
+        # if interval_type == 'CDS' and phase not in ('0', '1', '2'):
+        #     error = 'GFF intervals of type CDS must have phase of\
+        #         "0", "1" or "2"'
+        #     if line_number:
+        #         error = f'{error}, gff line {line_number}'
+        #         raise ValueError(error)
 
         interval = cls(seqid=seqid, source=source, interval_type=interval_type,
                        start=start, end=end, score=score, strand=strand,
@@ -212,14 +299,19 @@ class Interval():
 
         for attr_key in cls._predefined_attributes:
             attr_value = attributes[attr_key]
+
+            # ID must always be a single value, use random number if not set
+            if attr_key == 'ID':
+                if not attr_value:
+                    attr_value = str(uuid.uuid4())
+                else:
+                    attr_value = attr_value[0]
+
             if attr_value:
                 interval[attr_key] = attr_value
             del attributes[attr_key]
 
         interval.attributes = attributes
-
-        if not interval.ID:
-            interval.ID = uuid.uuid4()
 
         return interval
 
@@ -233,12 +325,20 @@ def parse_gff_attribute_string(gff_attribute_string):
     """
     attributes = defaultdict(list)
     for string_part in gff_attribute_string.split(';'):
-        key, value = string_part.split('=')
+        if not string_part:
+            continue
+        try:
+            key, value = string_part.split('=', maxsplit=1)
+        except Exception as e:
+            print(gff_attribute_string, string_part)
+            raise Exception(e)
         # The gff spec lists the predefined attribute fields as starting with
         # a capital letter, but we process in lowercase so we don't miss
         # anything from poorly formatted files. When writing to gff we convert
         # back to a capital
-        key = key.lower()
+        # EXCEPT FOR THE ID ATTRIBUTE, since lowercase id is reserved in python
+        if key != 'ID':
+            key = key.lower()
         for value_part in value.split(','):
             attributes[key].append(value_part)
     return attributes
